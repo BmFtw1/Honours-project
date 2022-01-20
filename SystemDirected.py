dbb32cd01a7ad1f988fdfec4165eedc6f0f181bd
Baoyan Si <sibaoyan@gmail.com>
2022-01-20 17:28:55 +0000

    operational version 1 chatbot, badly operative

--- SystemDirected.py
+++ SystemDirected.py
@@ -1,42 +1,52 @@
+# import necessary libraries
 import io
 import random
-import string # to process standard python strings
+import string  # to process standard python strings
 import warnings
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import warnings
+
 warnings.filterwarnings('ignore')
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-nltk.download('popular', quiet=True) # for downloading packages
+
+nltk.download('popular', quiet=True)  # for downloading packages
 
 # uncomment the following only the first time
-nltk.download('punkt') # first-time use only
-nltk.download('wordnet') # first-time use only
+# nltk.download('punkt') # first-time use only
+# nltk.download('wordnet') # first-time use only
 
 
-#Reading in the corpus
-with open('chatbot.txt','r', encoding='utf8', errors ='ignore') as fin:
+# Reading in the corpus
+with open('H:\Honours-project\WikiQACorpus\WikiQA-test.txt','r', encoding='utf8', errors ='ignore') as fin:
     raw = fin.read().lower()
-
-#TOkenisation
-sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences
-word_tokens = nltk.word_tokenize(raw)# converts to list of words
+    
+# TOkenisation
+sent_tokens = nltk.sent_tokenize(raw)  # converts to list of sentences
+word_tokens = nltk.word_tokenize(raw)  # converts to list of words
 
 # Preprocessing
 lemmer = WordNetLemmatizer()
+
+
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
+
+
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 
 # Keyword Matching
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
 
 def greeting(sentence):
     """If user's input is a greeting, return a greeting response"""
@@ -47,39 +57,39 @@ def greeting(sentence):
 
 # Generating response
 def response(user_response):
-    robo_response=''
+    robo_response = ''
     sent_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
-    idx=vals.argsort()[0][-2]
+    idx = vals.argsort()[0][-2]
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
-    if(req_tfidf==0):
-        robo_response=robo_response+"I am sorry! I don't understand you"
+    if (req_tfidf == 0):
+        robo_response = robo_response + "I am sorry! I don't understand you"
         return robo_response
     else:
-        robo_response = robo_response+sent_tokens[idx]
+        robo_response = robo_response + sent_tokens[idx]
         return robo_response
 
 
-flag=True
-print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
-while(flag==True):
+flag = True
+print("ROBO: Hello, nice to meet you!")
+while (flag == True):
     user_response = input()
-    user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
+    user_response = user_response.lower()
+    if (user_response != 'bye'):
+        if (user_response == 'thanks' or user_response == 'thank you'):
+            flag = False
             print("ROBO: You are welcome..")
         else:
-            if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
+            if (greeting(user_response) != None):
+                print("ROBO: " + greeting(user_response))
             else:
-                print("ROBO: ",end="")
+                print("ROBO: ", end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
-        flag=False
+        flag = False
         print("ROBO: Bye! take care..")
